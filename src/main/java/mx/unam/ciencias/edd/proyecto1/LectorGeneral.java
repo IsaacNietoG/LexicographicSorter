83e4d0214ba5341a265d8cfe952c78b741f70579
MrTaiChi <isaaniet@gmail.com>
2024-03-21 12:46:09 -0600

    feat: Moves all the reading methods to its own class, for better readabilit / Mueve todos los metodos de lectura a su propia clase, para mejor legibilidad

---
+++ src/main/java/mx/unam/ciencias/edd/proyecto1/LectorGeneral.java
@@ -0,0 +1,74 @@
+package mx.unam.ciencias.edd.proyecto1;
+
+import java.io.InputStreamReader;
+import mx.unam.ciencias.edd.Lista;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
+/**
+ * LectorGeneral
+ *
+ * Esta clase contiene los métodos que utillizaremos para leer los archivos y la entrada estandar que recibiremos del usuario en el proyecto.
+ *
+ * Recibe en su constructor dos objetos, uno siendo el {@link java.io.InputStreamReader} que tiene
+ * la entrada estándar del programa. (El system.in) y una lista de objetos File, que serán los archivos
+ * de donde extraeremos el texto.
+ *
+ * De ambas fuentes, junta todo en una lista de objetos StringOrdenables compuesta dentro de la
+ * instancia
+ */
+public class LectorGeneral {
+    private Lista<File> archivos;
+    private InputStreamReader standardIn;
+    private Lista<StringOrdenable> renglones;
+
+    public LectorGeneral(InputStreamReader standardIn, Lista<File> archivos){
+        this.archivos = archivos;
+        this.standardIn = standardIn;
+    }
+
+    public void leerEntradaEstandar(){
+        BufferedReader reader = new BufferedReader(standardIn);
+        String renglon;
+        try{
+            while((renglon = reader.readLine()) != null){
+                renglones.agrega(new StringOrdenable(renglon));
+            }
+        reader.close();
+        }catch (IOException e){
+            System.err.println("El archivo o los archivos recibidos por entrada estandar no son legibles, omitiendo...");
+        }
+    }
+
+    /**
+     *  Lee todos los archivos que existen en la Lista archivos y vacia sus renglones en la
+     *  lista de renglones
+     *  */
+    public void leerArchivos(){
+        for(File input : archivos){
+            BufferedReader reader;
+            try{
+                reader = new BufferedReader(new FileReader(input));
+            }catch(FileNotFoundException e){
+                System.err.printf("Uno de los archivos indicados como parametro no pudo leerse, omitiendo...");
+                continue;
+            }
+            String renglon;
+            try{
+                while((renglon = reader.readLine()) != null){
+                    renglones.agrega(new StringOrdenable(renglon));
+                }
+                reader.close();
+            }catch (IOException e){
+                System.err.println("El archivo o los archivos recibidos por argumento no son legibles, omitiendo...");
+            }
+        }
+    }
+
+    public Lista<StringOrdenable> getLista(){
+        return this.renglones;
+    }
+}
--- src/main/java/mx/unam/ciencias/edd/proyecto1/proyecto1.java
+++ src/main/java/mx/unam/ciencias/edd/proyecto1/proyecto1.java
@@ -1,11 +1,8 @@
 package mx.unam.ciencias.edd.proyecto1;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 import mx.unam.ciencias.edd.Lista;
 
@@ -77,34 +74,13 @@ public class proyecto1 {
             }
         }
 
-        //Comenzamos a reunir nuestras fuentes en una sola lista.
+        //Comenzamos a reunir nuestras fuentes en una sola lista. Para esto usamos la clase LectorGeneral
 
-        //Para los archivos ingresados como parametro
-        Lista<String> renglones = new Lista<>();
-        for(File input : archivos){
-            Scanner scanner;
-            try{
-                scanner = new Scanner(input);
-            }catch(FileNotFoundException e){
-                System.err.printf("Uno de los archivos indicados como parametro no pudo leerse, omitiendo...");
-                continue;
-            }
-            while(scanner.hasNext()){
-                renglones.agrega(scanner.useDelimiter("\n").next());
-            }
-        }
+        LectorGeneral lector = new LectorGeneral(new InputStreamReader(System.in), archivos);
+        lector.leerEntradaEstandar();
+        lector.leerArchivos();
 
-        //Para los ingresados como entrada estandar
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String renglon;
-        try{
-            while((renglon = reader.readLine()) != null){
-                renglones.agrega(renglon);
-            }
-        reader.close();
-        }catch (IOException e){
-            System.err.println("El archivo o los archivos recibidos por entrada estandar no son legibles, omitiendo...");
-        }
+        Lista<StringOrdenable> listaRenglones = lector.getLista();
 
 
     }
